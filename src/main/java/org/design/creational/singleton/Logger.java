e4aa1e97beb2e71e43870ba6740668eaa575cf8a
Bharadwaj Ghadiam <bharadwaj.ghadiam@arcserve.com>
2025-07-08 22:22:41 +0530

    logger design pattern

---
+++ src/main/java/org/design/creational/singleton/Logger.java
@@ -0,0 +1,15 @@
+package org.design.creational.singleton;
+
+public interface Logger {
+    void log(LogLevel level, String message);
+
+    void setLogFile(String filePath);
+
+    String getLogFile();
+
+    // Flush the log entries to the file
+    void flush();
+
+    // Close the logger and release any resources
+    void close();
+}
---
+++ src/main/java/org/design/creational/singleton/LoggerServiceImpl.java
@@ -0,0 +1,85 @@
+package org.design.creational.singleton;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoggerServiceImpl implements Logger {
+
+    private static Logger instance;
+    private PrintWriter printWriter;
+    private String logFilePath;
+
+    private LoggerServiceImpl() {
+
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (LoggerServiceImpl.class) {
+                if (instance == null) {
+                    instance = new LoggerServiceImpl();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void log(LogLevel level, String message) {
+        if (printWriter == null) {
+            throw new IllegalStateException("Log file is not set. Please set the log file before logging.");
+        }
+        try {
+            String formattedMessage = String.format("%s [%s]: %s", java.time.LocalDateTime.now(), level.name(), message);
+            printWriter.println(formattedMessage);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write to log file: " + logFilePath, e);
+        }
+    }
+
+    @Override
+    public void setLogFile(String filePath) {
+        File file = new File(filePath);
+        this.logFilePath = filePath;
+        if (printWriter != null) {
+            printWriter.close();
+        }
+        try {
+            if (!file.exists()) {
+               boolean result= file.createNewFile();
+               if(!result) {
+                   throw new IOException("Failed to create log file: " + filePath);
+               }
+            }
+            this.printWriter = new PrintWriter(new FileWriter(file, true), true);
+            printWriter.println("Log file created at: " + filePath + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create or open log file: " + filePath, e);
+        }
+
+    }
+
+    @Override
+    public String getLogFile() {
+        return this.logFilePath;
+    }
+
+    @Override
+    public void flush() {
+        if (printWriter == null) {
+            throw new IllegalStateException("Log file is not set. Please set the log file before flushing.");
+        }
+        printWriter.flush();
+    }
+
+    @Override
+    public void close() {
+        if (printWriter == null) {
+            throw new IllegalStateException("Log file is not set. Please set the log file before closing.");
+        }
+        printWriter.close();
+
+    }
+}
